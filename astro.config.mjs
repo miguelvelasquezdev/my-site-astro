25143d83295742eca5555c8f333aaafd1a8bdef1
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-04-09 19:04:30 -0500

    cal.com added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,5 +17,8 @@ export default defineConfig({
     }),
     tailwind(),
     react(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
   ],
 });
