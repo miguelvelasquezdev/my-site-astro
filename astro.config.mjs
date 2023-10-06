5797a0034c30c98868addf1bd21646cc0189d3ef
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-10-06 13:41:47 -0500

    SST added and svelte was replaced by astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,11 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+import aws from "astro-sst/lambda";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import robotsTxt from "astro-robots-txt";
-import vue from "@astrojs/vue";
+import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
-import solidJs from "@astrojs/solid-js";
-import partytown from "@astrojs/partytown";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://miguelvelasquez.dev",
-  integrations: [
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "dracula",
-      },
-      gfm: false,
-    }),
-    tailwind(),
-    react(),
-    sitemap(),
-    robotsTxt(),
-    vue(),
-    svelte(),
-    solidJs(),
-  ],
+  output: "server",
+  adapter: aws(),
+  integrations: [tailwind(), mdx(), svelte()],
 });
