e59cf8deb78c27933245143a63caf2bbfa97effe
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-11-25 17:32:30 -0500

    fix: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,22 +6,18 @@ import robotsTxt from "astro-robots-txt";
 import svelte from "@astrojs/svelte";
 import solid from "@astrojs/solid-js";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://miguelvelasquez.dev",
-  integrations: [
-    solid(),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "dracula",
-      },
-      gfm: false,
-    }),
-    tailwind(),
-    sitemap(),
-    robotsTxt(),
-    svelte(),
-  ],
+  integrations: [solid(), mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "dracula"
+    },
+    gfm: false
+  }), tailwind(), sitemap(), robotsTxt(), svelte()],
+  adapter: vercel()
 });
